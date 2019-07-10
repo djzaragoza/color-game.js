fda578a7d9ef220db6f7c81247cf8d86959677d0
DJ Zaragoza <zaragoza0126@gmail.com>
2019-07-09 22:44:13 -0700

    added reset game and random color generator

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 var numSquares = 6;
 var pickedColor;
 var squares = document.querySelectorAll(".square");
-var messageDisplay = document.querySelector("#message");
+var systemMessageDisplay = document.querySelector("#default-code");
+var userMessageDisplay = document.querySelector("#user-code");
 var resetButton = document.querySelector("#reset");
 var modeButtons = document.querySelectorAll(".mode");
+var header = document.querySelector('#header');
 
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 255, 0)",
-    "rgb(255, 0, 255)",
-    "rgb(0, 255, 255)"
-];
+var colors = generateRandomColors(6);
+pickedColor = pickColor();
 
-pickedColor = colors[3];
+// Reset Game...
+resetButton.addEventListener('click', function () {
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    systemMessageDisplay.innerHTML = pickedColor;
+    header.style.backgroundColor = 'steelblue';
+    userMessageDisplay.innerHTML = '';
+    for (i = 0; i <= squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+})
+
+
+systemMessageDisplay.innerHTML = pickedColor;
 
 for (i = 0; i < squares.length; i++) {
     //apply background colour to all the squares...
@@ -22,14 +31,16 @@ for (i = 0; i < squares.length; i++) {
 
     //enable click event on each square.....
     squares[i].addEventListener('click', function () {
+        //show user the current value....
+        userMessageDisplay.innerHTML = this.style.backgroundColor;
         //if the user selected the right colour....
         var clickedColor = this.style.backgroundColor;
         //check if the selected colour matches the default colour...
         if (pickedColor === clickedColor) {
+            header.style.backgroundColor = pickedColor;
             changeColors(pickedColor);
-            messageDisplay.text = "You are good at guessing!";
         }
-        //if the user user selected wrong colour....
+        //if the user selected wrong colour....
         else {
             this.style.backgroundColor = "#232323";
             messageDisplay.text = "Wrong Choice!";
@@ -41,4 +52,25 @@ function changeColors(color) {
     for (i = 0; i <= squares.length; i++) {
         squares[i].style.backgroundColor = color;
     }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    console.log("pickColor");
+    return colors[random];
+}
+
+function randomColors() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + b + ", " + g + ")";
+}
+
+function generateRandomColors(num) {
+    var arr = [];
+    for (i = 0; i < num; i++) {
+        arr.push(randomColors());
+    }
+    return arr;
 }
