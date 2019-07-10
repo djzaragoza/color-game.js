81bde709a79a7fdef0b3fbf164046a2618ea0d96
DJ Zaragoza <zaragoza0126@gmail.com>
2019-07-09 22:37:04 -0700

    finished with all files.  need to debug prior to final testing

--- app.js
+++ app.js
@@ -1,47 +1,35 @@
-var numSquares = 6; // numSquares variables stores number of squares that will be available in the game as per the mode.
-var colors = []; // is an empty array which contains the random six or three RGB color
-var pickedColor; //color/option block selected hby the user upon every click
-var squares = document.querySelectorAll(".square"); // array of all the squares on the page that are available as options
-var resetButton = document.querySelector("#reset"); // variable is the "new game" button in the control panel
-var modeButtons = document.querySelectorAll(".mode"); //array which has easy and hard mode buttons
+var numSquares = 6;
+var pickedColor;
+var squares = document.querySelectorAll(".square");
+var messageDisplay = document.querySelector("#message");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
 
 var colors = [
     "rgb(255, 0, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 255, 0)",
     "rgb(255, 0, 255)",
-    "rgb(255, 225, 0)",
-    "rgb(255, 0, 255)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 255, 0)"
+    "rgb(0, 255, 255)"
 ];
-var squares = document.querySelectorAll(.squares);
-for (i = 0; i < squares.length; i++) {
-    squares.style.backgroundColor = colors[i]
-}
-
-//enabling click functionality
-for(i=0; i<= squares.length; i++) {
-    squares[i].addEventListeners('click', function() {
-        alert('option was clicked');
-    });
-}
-//if correct block is clicked do something ....
-//if wrong block is clicked do something ....
-
 
 pickedColor = colors[3];
 
-for (i=0; i <= squares.length; i++) {
-    //apply background color to all squares ....
+for (i = 0; i < squares.length; i++) {
+    //apply background colour to all the squares...
     squares[i].style.backgroundColor = colors[i]
-    //enable click event on each square
-    squares[i].addEventListener('click', function() {
-        //if the user selected the right color ....
+
+    //enable click event on each square.....
+    squares[i].addEventListener('click', function () {
+        //if the user selected the right colour....
         var clickedColor = this.style.backgroundColor;
-        //check if the selected color matches default color
+        //check if the selected colour matches the default colour...
         if (pickedColor === clickedColor) {
             changeColors(pickedColor);
+            messageDisplay.text = "You are good at guessing!";
         }
-        //if the user selected wrong color ....
+        //if the user user selected wrong colour....
         else {
             this.style.backgroundColor = "#232323";
             messageDisplay.text = "Wrong Choice!";
@@ -49,3 +37,8 @@ for (i=0; i <= squares.length; i++) {
     })
 };
 
+function changeColors(color) {
+    for (i = 0; i <= squares.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
