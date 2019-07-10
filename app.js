e188d4a2463d810dc6ca19fb6aaafdcb8e38fae2
DJ Zaragoza <zaragoza0126@gmail.com>
2019-07-09 21:56:23 -0700

    finished app.js file

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
-<!DOCTYPE html>
-<html>
+var numSquares = 6;
+var colors = [];
+var pickedColor;
+var squares = document.querySelectorAll(".square");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
 
-<head>
-    <title>Color Game</title>
-    <link rel="stylesheet" type="text/css" href="app.css"></link>
-</head>
-
-
-<body>
-
-
-<h1>The Great<br>
-<span id="colorDisplay">RGB</span></br>
-Color Game
-</h1>
-
-<div id="stripe">
-
-    <button id="reset">New Colors</button>
-    <span id="message"></span>
-    <button class="mode">Easy</button>
-    <button class="mode selected">Hard</button> 
-</div>
-
-<div id="container">
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-
-</div>
-
-<script type="text/javascript" src="app.js"></script>
-
-</body>
-
-</html>
